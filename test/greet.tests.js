3df5753302d131dc2152fb553ac2e4a327eae84c
GarethW1994 <williams.gareth0@gmail.com>
2017-04-07 12:50:52 +0200

    save changes

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,15 +1,15 @@
 //mocha tests
 var assert = require('assert');
 //call greet module
-const greetMessage = require('../functions/greet');
+const greetMessage = require('../greet');
 
 //test the greet function
 describe('greet()', function() {
     it("should greet Gareth correctly", function(){
-      assert.equal(greetMessage('Gareth'), 'Hello, Gareth');  
+      assert.equal(greetMessage('Gareth'), 'Hello, Gareth');
     });
-    
+
     it ("should greet Andrew correctly", function() {
-       assert.equal(greetMessage('Andrew'), 'Hello, Andrew') 
+       assert.equal(greetMessage('Andrew'), 'Hello, Andrew')
     });
 });
--- test/isFromBellville.tests.js
+++ test/isFromBellville.tests.js
@@ -1,15 +1,15 @@
 //mocha assert
 const assert = require('assert');
 //require function module
-const isFromBellvilleTest = require('../functions/isFromBellville');
+const isFromBellvilleTest = require('../isFromBellville');
 
 //mocha testing
 describe('isFromBellville()', function() {
    it ("should return 'false' when given a numberplate not from Bellville", function() {
-      assert.equal(isFromBellvilleTest('CA 1234', 'CY'), false); 
+      assert.equal(isFromBellvilleTest('CA 1234', 'CY'), false);
    });
-    
+
    it ("should return 'true' when given a numberplate from Bellville", function() {
-      assert.equal(isFromBellvilleTest('CY 4356', 'CY'), true); 
+      assert.equal(isFromBellvilleTest('CY 4356', 'CY'), true);
    });
 });
